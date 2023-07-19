6dec81282163dc80d95a27d8cb628656974e9ff3
Zachary Juang <zachary822@me.com>
2023-07-19 01:07:42 -0400

    Revert "less insane implementation"
    This reverts commit 92de2959687f4d448fb74a76cb59c07498d5486b.

--- src/Lib/Hotp.hs
+++ src/Lib/Hotp.hs
@@ -10,16 +10,14 @@ import Data.ByteArray (convert)
 import Data.ByteString (ByteString)
 import Data.ByteString qualified as B
 import Data.ByteString.Lazy qualified as BL
+import Text.Printf (printf)
 
 -- | calculate HOTP code from seed and moving factor
 hotp :: ByteString -> ByteString -> Int -> String
-hotp key counter digits
-  | digits > codeLen = take (digits - codeLen) (repeat '0') ++ codeStr
-  | otherwise = drop (codeLen - digits) codeStr
- where
-  digest = hmacGetDigest $ hmac key counter :: Digest SHA1
-  hash = B.unpack $ convert digest
-  offset = fromIntegral $ last hash .&. 0x0f :: Int
-  codeInt = fromIntegral $ runGet getWord32be (BL.pack $ take 4 . drop offset $ hash) .&. 0x7fffffff :: Int
-  codeStr = show codeInt
-  codeLen = length codeStr
+hotp key counter digits = printf ("%0" ++ show digits ++ "u") codeStr
+  where
+    digest = hmacGetDigest $ hmac key counter :: Digest SHA1
+    hash = B.unpack $ convert digest
+    offset = fromIntegral $ last hash .&. 0x0f :: Int
+    codeInt = fromIntegral $ runGet getWord32be (BL.pack $ take 4 . drop offset $ hash) .&. 0x7fffffff :: Int
+    codeStr = codeInt `mod` (10 ^ digits)
